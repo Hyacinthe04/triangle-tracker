82810c79f58a2b7fd5f25d5abaec3d891060724b
Hyacinthe04 <hyacinthen04@gmail.com>
2018-12-17 13:19:20 +0200

    change2

--- tracker.js
+++ tracker.js
@@ -4,39 +4,22 @@ function seeTriangle() {
   var side3 = document.getElementById("Side3").value;
 
 
-if (thisIsNotTriangle(side1,side2,side3)) {
-  if (side1 === side2 && side1 === side3) {
-      alert("equilateral");
+  if(side1===side2 && side2===side3 && side2===side3){
+    alert("Triangle is Equilateral ");
     }
-    else if (side1 === side2 || side1 === side3|| side2 === side3){
-      alert("isosceles");
+    else if(side1===side2 || side1===side3|| side3===side2){
+    alert("Triangle is Isosceles");
     }
-    else {
-      alert("scalene");
+    else if(((side1+side2) > side3) || ((side1+side3) > side2) ||((side2+side3) > side1) && (firstSide!=secondSide) && (firstSide!=thirdSide) && (secondSide!=thirdSide)) {
+    alert("scalene");
     }
-} 
-
-
-else {
-  alert("not a triangle");
-}
-  
-}
-
-function thisIsNotTriangle(side1,side2,side3) {
-if ( (side1 + side2) <= side3 ) {
-    return false;
-}
-else if ( (side1 + side3) <= side1 ) {
-  return false;
-}
-else if ( (side1 + side3) <= side2 ) {
-  return false;
-}
-else if (!(side1) || !(side2) || !(side3)){
-  return false;
-}
-else {
-  return true;
-}
-}
+    else if(((side1+side2) <= side3) ||((side1+side3)<=side2) ||((side2+side3) <=side1) ){
+      alert("no triangle");
+    }
+    else{
+      alert("invalid");
+    }
+  }
+    
+    
+    
