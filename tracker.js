278b4d26c8a077fa139e83692b22ebec11cc45cc
Hyacinthe04 <hyacinthen04@gmail.com>
2018-12-18 08:07:03 +0200

    change7

--- tracker.js
+++ tracker.js
@@ -1,25 +1,25 @@
 function seeTriangle() {
-  var side1 = document.getElementById("Side1").value;
-  var side2 = document.getElementById("Side2").value;
-  var side3 = document.getElementById("Side3").value;
-
-
-  if(side1===side2 && side2===side3 && side2===side3){
+  var sideA = parseInt(document.getElementById("sideA").value);
+  var sideB = parseInt(document.getElementById("sideB").value);
+  var sideC = parseInt(document.getElementById("sideC").value);
+   if((sideA+sideB)> sideC && (sideA+sideC)>sideB && (sideB+sideC)>sideA) {
+     alert("triangle");
+  if(sideA===sideB && sideB===sideC && sideB===sideC){
     alert("Triangle is Equilateral ");
     }
-    else if(side1===side2 || side1===side3|| side3===side2){
+   else if(sideA===sideB || sideA===sideC || sideC===sideB){
     alert("Triangle is Isosceles");
     }
-    else if(((side1+side2) > side3) || ((side1+side3) > side2) ||((side2+side3) > side1) && (firstSide!=secondSide) && (firstSide!=thirdSide) && (secondSide!=thirdSide)) {
-    alert("scalene");
+    else{
+      alert("Triangle is Scalene");
     }
-    else if(((side1+side2) <= side3) ||((side1+side3)<=side2) ||((side2+side3) <=side1) ){
-      alert("no triangle");
     }
     else{
-      alert("invalid");
+    alert("Values do not match a Triangle");
     }
-  }
+    };
+    
+    
     
     
     
